ae6f5cf73d42f170f7efe7123e5b72c1f4729e57
Sandani Gunawardhana <sandanigunawardhana@gmail.com>
2024-11-15 01:00:50 +0530

    retrieve company requests for admin

--- src/components/Admin/CompanyRequests.jsx
+++ src/components/Admin/CompanyRequests.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Card, CardBody, Button, Tooltip, Chip} from "@nextui-org/react";
+import React, { useEffect, useState } from 'react'
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Card, CardBody, Button, Tooltip, Chip, useDisclosure} from "@nextui-org/react";
 import { FaRegEye } from "react-icons/fa6";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { MdOutlineCancel } from "react-icons/md";
+import { getToken } from '../../pages/Auth/Auth';
 
 const CompanyRequests = () => {
+    const { isOpen, onOpenChange } = useDisclosure();
+    const [requests, setRequests] = useState([]);
+    const [selectedRequest, setSelectedRequest] = useState(null); 
+
+    useEffect(() => {
+        const token = getToken();
+        console.log(token);
+    
+        fetch('http://localhost:8080/admin/companyRequests', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Failed to fetch companies');
+            }
+            return response.json();
+          })
+          .then(data => {
+            setRequests(data);
+          })
+          .catch(error => {
+            console.log('Error fetching companies:', error);
+          });
+      }, []);
+    
+      
+      const handleOpen = (request) => {
+        setSelectedRequest(request);  
+        onOpenChange(true); 
+      };
+
+
 
   return (
     <Card>
@@ -19,152 +56,69 @@ const CompanyRequests = () => {
                     <TableColumn>ACTIONS</TableColumn>
                     </TableHeader>
                     <TableBody>
-                    <TableRow key="1">
-                        <TableCell>Tony Reichert</TableCell>
-                        <TableCell>CEO</TableCell>
-                        <TableCell><Chip color="warning" size='sm' variant="flat">Pending</Chip></TableCell>
-                        <TableCell>
-                            <div className="relative flex items-center gap-2">
+                        {requests.map((request, index) => (
+                            <TableRow key={index + 1}>
+                            <TableCell>{request.companyName}</TableCell>
+                            <TableCell>{request.location}</TableCell>
+                            <TableCell><Chip color="warning" variant="flat">{request.status}</Chip></TableCell>
+                            <TableCell>
+                                <div className="relative flex items-center gap-2">
                                 <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                    <span
+                                    className="text-lg text-default-400 cursor-pointer active:opacity-50"
+                                    onClick={() => handleOpen(request)} 
+                                    >
                                     <FaRegEye />
-                                </span>
+                                    </span>
                                 </Tooltip>
-                                <Tooltip color='success' className='text-white' content="Accept Company">
-                                <span className="text-lg text-success cursor-pointer active:opacity-50">
+                                <Tooltip color="success" content="Reject Company">
+                                    <span className="text-lg text-success cursor-pointer active:opacity-50">
                                     <FaRegCheckCircle />
-                                </span>
+                                    </span>
                                 </Tooltip>
                                 <Tooltip color="danger" content="Reject Company">
-                                <span className="text-xl text-danger cursor-pointer active:opacity-50">
+                                    <span className="text-xl text-danger cursor-pointer active:opacity-50">
                                     <MdOutlineCancel />
-                                </span>
+                                    </span>
                                 </Tooltip>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow key="2">
-                        <TableCell>Zoey Lang</TableCell>
-                        <TableCell>Technical Lead</TableCell>
-                        <TableCell><Chip color="success" size='sm' variant="flat">Accepted</Chip></TableCell>
-                        <TableCell>
-                            <div className="relative flex items-center gap-2">
-                                <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <FaRegEye />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color='success' className='text-white' content="Accept Company">
-                                <span className="text-lg text-success cursor-pointer active:opacity-50">
-                                    <FaRegCheckCircle />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color="danger" content="Reject Company">
-                                <span className="text-xl text-danger cursor-pointer active:opacity-50">
-                                    <MdOutlineCancel />
-                                </span>
-                                </Tooltip>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow key="3">
-                        <TableCell>Jane Fisher</TableCell>
-                        <TableCell>Senior Developer</TableCell>
-                        <TableCell><Chip color="warning" size='sm' variant="flat">Pending</Chip></TableCell>
-                        <TableCell>
-                            <div className="relative flex items-center gap-2">
-                                <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <FaRegEye />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color='success' className='text-white' content="Accept Company">
-                                <span className="text-lg text-success cursor-pointer active:opacity-50">
-                                    <FaRegCheckCircle />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color="danger" content="Reject Company">
-                                <span className="text-xl text-danger cursor-pointer active:opacity-50">
-                                    <MdOutlineCancel />
-                                </span>
-                                </Tooltip>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow key="4">
-                        <TableCell>William Howard</TableCell>
-                        <TableCell>Community Manager</TableCell>
-                        <TableCell><Chip color="danger" size='sm' variant="flat">Rejected</Chip></TableCell>
-                        <TableCell>
-                            <div className="relative flex items-center gap-2">
-                                <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <FaRegEye />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color='success' className='text-white' content="Accept Company">
-                                <span className="text-lg text-success cursor-pointer active:opacity-50">
-                                    <FaRegCheckCircle />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color="danger" content="Reject Company">
-                                <span className="text-xl text-danger cursor-pointer active:opacity-50">
-                                    <MdOutlineCancel />
-                                </span>
-                                </Tooltip>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow key="5">
-                        <TableCell>Tony Reichert</TableCell>
-                        <TableCell>CEO</TableCell>
-                        <TableCell><Chip color="warning" size='sm' variant="flat">Pending</Chip></TableCell>
-                        <TableCell>
-                            <div className="relative flex items-center gap-2">
-                                <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <FaRegEye />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color='success' className='text-white' content="Accept Company">
-                                <span className="text-lg text-success cursor-pointer active:opacity-50">
-                                    <FaRegCheckCircle />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color="danger" content="Reject Company">
-                                <span className="text-xl text-danger cursor-pointer active:opacity-50">
-                                    <MdOutlineCancel />
-                                </span>
-                                </Tooltip>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow key="6">
-                        <TableCell>Zoey Lang</TableCell>
-                        <TableCell>Technical Lead</TableCell>
-                        <TableCell><Chip color="warning" size='sm' variant="flat">Pending</Chip></TableCell>
-                        <TableCell>
-                            <div className="relative flex items-center gap-2">
-                                <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <FaRegEye />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color='success' className='text-white' content="Accept Company">
-                                <span className="text-lg text-success cursor-pointer active:opacity-50">
-                                    <FaRegCheckCircle />
-                                </span>
-                                </Tooltip>
-                                <Tooltip color="danger" content="Reject Company">
-                                <span className="text-xl text-danger cursor-pointer active:opacity-50">
-                                    <MdOutlineCancel />
-                                </span>
-                                </Tooltip>
-                            </div>
-                        </TableCell>
-                    </TableRow>
+                                </div>
+                            </TableCell>
+                            </TableRow>
+                        ))}
                     </TableBody>
                 </Table>
+
+                <Modal size='4xl' placement='auto' isOpen={isOpen} onOpenChange={onOpenChange}>
+                    <ModalContent style={{ marginLeft: '18rem' }}>
+                    {(onClose) => (
+                        <>
+                        <ModalHeader className="flex flex-col gap-1 text-purple1 text-2xl">Company Details</ModalHeader>
+                        <ModalBody className='flex flex-row'>
+                            <div className='w-1/2'>
+                                {selectedRequest && (
+                                <>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Company Name:<br/></span> {selectedRequest.companyName}</p>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Industry:<br/></span> {selectedRequest.industryType}</p>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Business Reg No:<br/></span> {selectedRequest.regNo}</p>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Email:<br/></span> {selectedRequest.email}</p>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Website:<br/></span> <a className='text-blue-600' href={selectedRequest.companyWebsite} target="_blank" rel="noopener noreferrer">Click Here</a></p>
+                                </>
+                                )}
+                            </div>
+                            <div className='w-1/2'>
+                                {selectedRequest && (
+                                <>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Company Logo:<br/></span> {selectedRequest.companyName}</p>
+                                    <p className='mb-3'><span className='text-purple2 font-medium'>Business Registration:<br/></span> {selectedRequest.industryType}</p>
+                                </>
+                                )}
+                            </div>                
+                        </ModalBody>
+                        </>
+                    )}
+                    </ModalContent>
+                </Modal>
+
                 <div className="flex justify-between">
                     <Button
                     size="sm"
